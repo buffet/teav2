e7e229f0dc77381c4fabb84c639bec7a6ca7241a
countingsort <niclas@countingsort.com>
2018-06-12 15:56:28 +0200

    Usage n command parser
    - fixed docopt
    - added usage string

--- src/teav2.cpp
+++ src/teav2.cpp
@@ -1,2 +1,25 @@
+#include <docopt/docopt.h>
+
+#include <iostream>
+
+#define VERSION_STRING "1.0"
+
+static const char USAGE[] =
+R"(Usage:
+    teav2 send RECIPIENT (-m | -f) OBJECT
+    teav2 [OPTIONS]
+
+Options:
+    -h --help         Show this screen
+    -v --version      Show version
+    -i --interactive  Start in interactive mode
+)";
+
 int main(int argc, char *argv[]) {
+    auto args = docopt::docopt(
+        USAGE,
+        { argv + 1, argv + argc },
+        true,
+        "teav2 v" VERSION_STRING
+    );
 }
